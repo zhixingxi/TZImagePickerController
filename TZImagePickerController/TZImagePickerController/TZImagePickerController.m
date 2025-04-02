cc9fdbdabc94f09efa2d41ff3957c2a6d0202adc
mql <>
2025-04-02 10:47:28 +0800

    相册视频超过时长置灰

--- TZImagePickerController/TZImagePickerController/TZImagePickerController.m
+++ TZImagePickerController/TZImagePickerController/TZImagePickerController.m
@@ -1056,6 +1056,9 @@
             notSelectable = true;
         }
     }
+    if (!notSelectable && model.asset.mediaType == PHAssetMediaTypeVideo && model.asset.duration > tzImagePickerVc.videoMaximumDuration) {
+            notSelectable = true;
+    }
     return notSelectable;
 }
 
